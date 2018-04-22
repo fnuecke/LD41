9a32e95302485cf526bf9e1804dbb75caebbb042
Florian Nücke <florian@nuecke.de>
2018-04-22 09:19:14 +0200

    Added list shuffle extension method.

---
+++ Assets/Scripts/Extensions/ListExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace MightyPirates
+{
+    public static class ListExtensions
+    {
+        public static void Shuffle<T>(this List<T> self)
+        {
+            for (int i = 0; i < self.Count; i++)
+            {
+                int j = Random.Range(0, i + 1);
+                T tmp = self[i];
+                self[i] = self[j];
+                self[j] = tmp;
+            }
+        }
+    }
+}
