ea33accda0acac58425efb1de5cf91b37909bea0
Florian Nücke <florian@nuecke.de>
2018-04-22 09:19:03 +0200

    Fixed bound expansion not adjusting origin.

--- Assets/Scripts/Extensions/BoundsIntExtensions.cs
+++ Assets/Scripts/Extensions/BoundsIntExtensions.cs
@@ -6,7 +6,7 @@ namespace MightyPirates
     {
         public static BoundsInt Expand(this BoundsInt self, Vector3Int delta)
         {
-            return new BoundsInt(self.position, new Vector3Int(self.size.x + delta.x * 2, self.size.y + delta.y * 2, self.size.z + delta.z * 2));
+            return new BoundsInt(self.position - new Vector3Int(delta.x, delta.y, delta.z), new Vector3Int(self.size.x + delta.x * 2, self.size.y + delta.y * 2, self.size.z + delta.z * 2));
         }
     }
 }
